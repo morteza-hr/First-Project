569acb605de5faf051c0ca97cfa0a3ed6396f832
Mori <96136226+morteza-hr@users.noreply.github.com>
2021-12-15 12:34:08 +0330

    Update HelloWorld.js

--- src/component/HelloWorld.js
+++ src/component/HelloWorld.js
@@ -4,9 +4,7 @@ function Helloworld (){
     return(
         <div>
             <h1>
-            helloworld1ghghfhff
-			tfyfyjuf
-			gdfgfdgdfgfdgdfgkookgdfgdfgd
+            helloworld1
             </h1>
         </div>
           
